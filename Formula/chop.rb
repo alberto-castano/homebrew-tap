c072bdac64e8edff5a164ab77f0cd14e7b624adc
Alberto Castaño <bertocast@gmail.com>
2026-09-05 09:11:58 +0200

    fix: remove duplicate Cargo offline flag

--- Formula/chop.rb
+++ Formula/chop.rb
@@ -4,6 +4,7 @@ class Chop < Formula
   url "https://github.com/alberto-castano/chop/archive/refs/tags/v0.2.0.tar.gz"
   sha256 "f537a3474001f69b0caee2885d82d285dc430f445375426102658ba79ad7d758"
   license "MIT"
+  revision 1
   head "https://github.com/alberto-castano/chop.git", branch: "main"
 
   depends_on "rust" => :build
@@ -13,7 +14,7 @@ class Chop < Formula
   end
 
   def install
-    system "cargo", "install", "--offline", *std_cargo_args
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
